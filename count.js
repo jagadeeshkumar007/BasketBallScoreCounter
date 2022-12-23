28982f7c9522aa5e06c4875872c1d36a304fed99
jagadeeshkumar007 <107942144+jagadeeshkumar007@users.noreply.github.com>
2022-12-23 15:07:56 +0530

    Add files via upload

---
+++ count.js
@@ -0,0 +1,35 @@
+let hmsc = document.getElementById("hm")
+let gmsc = document.getElementById("gs")
+let count1=0
+let count2=0
+function n1()
+{
+    count1=count1+1
+    hmsc.innerText=count1
+}
+function n2()
+{
+    count1=count1+2
+    hmsc.innerText=count1
+}
+function n3()
+{
+    count1=count1+3
+    hmsc.innerText=count1
+}
+
+function n11()
+{
+    count2=count2+1;
+    gmsc.innerText=count2
+}
+function n12()
+{
+    count2=count2+2;
+    gmsc.innerText=count2
+}
+function n13()
+{
+    count2=count2+3;
+    gmsc.innerText=count2
+}
